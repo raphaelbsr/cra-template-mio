a9d6469521ec2d4201200959e79b01869ded29e7
raphaelbsr <raphael@datac.com.br>
2020-02-04 17:31:38 -0300

    first commit

---
+++ template/src/pages/Login/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+// import { Autenticacao } from "~/components/index";
+import { Container, Top, Bottom, LogoContent, LoginContent } from "./styles";
+import Logo from "~/assets/images/Logo.png";
+import Logo2x from "~/assets/images/Logo@2x.png";
+
+import { Autenticacao } from "mio-library-autenticacao";
+
+const Login = props => {
+  return (
+    <Container>
+      <Top>
+        <LogoContent>
+          <img src={Logo} srcSet={`${Logo} 1x, ${Logo2x} 2x`} alt="MIO" />
+        </LogoContent>
+      </Top>
+      <Bottom>
+        <LoginContent>
+          <Autenticacao
+            antesDeEntrar={() => {}}
+            depoisDeEntrar={status => {
+              if (status.status) {
+                props.history.push("/home");
+              }
+            }}
+          />
+        </LoginContent>
+      </Bottom>
+    </Container>
+  );
+};
+
+export default Login;
